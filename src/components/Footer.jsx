6e9451352585076546199a488baa6b31fd0c729a
KushibikiMashu <kushibiki3413@gmail.com>
2018-12-29 18:27:18 +0900

    Add App component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
-import FilterLink from './container/FilterLink'
+import FilterLink from '../containers/FilterLink'
 import { VisibilityFilters } from '../actions'
 
 const Footer = () => (
   <p>
-    Show: <FilterLink filter={VisibilityFilters.SHOW_ALL}>All</FilterLink>
-    {', '}
-    Show: <FilterLink filter={VisibilityFilters.SHOW_ACTIVE}>Active</FilterLink>
-    {', '}
-    Show:{' '}
+    <span>Show: </span>
+    <FilterLink filter={VisibilityFilters.SHOW_ALL}>All</FilterLink>
+    <FilterLink filter={VisibilityFilters.SHOW_ACTIVE}>Active</FilterLink>
     <FilterLink filter={VisibilityFilters.SHOW_COMPLETED}>Completed</FilterLink>
   </p>
 )
